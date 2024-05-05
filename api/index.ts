bdf1d7c8363d65c2d284097ad101fe131e317a56
AJ Stuyvenberg <astuyve@gmail.com>
2024-05-05 18:49:56 -0400

    process.uptime and cold start bool

--- api/index.ts
+++ api/index.ts
@@ -1,7 +1,22 @@
+const staticInitTime = Date.now();
+let isColdStart = true;
 import type {VercelRequest, VercelResponse} from '@vercel/node';
 
-export default function (request: VercelRequest, response: VercelResponse) {
+export default function (_request: VercelRequest, response: VercelResponse) {
+  let coldStartResult = false;
+  if (isColdStart) {
+    isColdStart = false;
+    coldStartResult = true;
+  }
+  const handlerRunTime = Date.now();
+  const resp = {
+    handlerRunTime,
+    staticInitTime,
+    coldStartResult,
+    processUptime: process.uptime(),
+  }
+
   response.status(200).json({
-    text: `Hello from ${request.url}`
+    resp 
   });
 }
